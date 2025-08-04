49068bec41ca4f348c22b89c6dbf952593439c98
Nana <nana@gmail.com>
2025-08-04 23:23:53 +0000

    Added Ticket Price Calculator

---
+++ TicketPriceCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the Ticket Price Calculator!");
+
+        Console.Write("Enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+
+        if (age <= 18 || age >= 45)
+        {
+            Console.WriteLine("Ticket price: GHC7");
+        }
+        else
+        {
+            Console.WriteLine("Ticket price: GHC10");
+        }
+    }
+}
+
