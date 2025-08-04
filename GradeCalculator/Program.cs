0dcd4d1aec6b30483ea1f7c6be89d6666549959e
Nana <nana@gmail.com>
2025-08-04 23:12:24 +0000

    Added Grade Calculator

---
+++ GradeCalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the Grade Calculator!");
+
+        Console.Write("Enter your grade (0 to 100): ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+
+        if (grade >= 90)
+            Console.WriteLine("You got an A!");
+        else if (grade >= 80)
+            Console.WriteLine("You got a B.");
+        else if (grade >= 70)
+            Console.WriteLine("You got a C.");
+        else if (grade >= 60)
+            Console.WriteLine("You got a D.");
+        else
+            Console.WriteLine("You got an F.");
+    }
+}
