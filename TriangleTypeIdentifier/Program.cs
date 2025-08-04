81946afded10dbac16c789eab5878fdf950faf5c
Nana <nana@gmail.com>
2025-08-04 23:29:45 +0000

    Added Triangle Type Identifier

---
+++ TriangleTypeIdentifier/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the Triangle Type Identifier!");
+
+        Console.Write("Enter length of side 1: ");
+        int side1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter length of side 2: ");
+        int side2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter length of side 3: ");
+        int side3 = int.Parse(Console.ReadLine());
+
+        if (side1 == side2 && side2 == side3)
+        {
+            Console.WriteLine("This is an Equilateral triangle.");
+        }
+        else if (side1 == side2 || side2 == side3 || side1 == side3)
+        {
+            Console.WriteLine("This is an Isosceles triangle.");
+        }
+        else
+        {
+            Console.WriteLine("This is a Scalene triangle.");
+        }
+    }
+}
